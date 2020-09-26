7dda723b9cb59ffcafd776d2d7f4ac00bfad8357
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-09-27 00:55:15 +0900

    iOS first commit

---
+++ iOS/labo_atte/labo_atte/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  labo_atte
+//
+//  Created by jun on 2020/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
